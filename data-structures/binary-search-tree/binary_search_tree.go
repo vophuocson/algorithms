7288d7af678e122da0e1cdfd22a7b106dd042e9a
son-vp1998 <son-vp@genkisystem.com>
2025-04-30 15:25:57 +0700

    int binary search tree and InOrderTreeWall function

--- data-structures/binary-search-tree/binary_search_tree.go
+++ data-structures/binary-search-tree/binary_search_tree.go
@@ -1,5 +1,7 @@
 package binarysearchtree
 
+import "fmt"
+
 type Node struct {
 	Parent *Node
 	Left   *Node
@@ -9,4 +11,12 @@ type Node struct {
 
 type Tree struct {
 	Root *Node
+}
+
+func InOrderTreeWalk(n *Node) {
+	if n != nil {
+		InOrderTreeWalk(n.Left)
+		fmt.Println(n.Key)
+		InOrderTreeWalk(n.Right)
+	}
 }
